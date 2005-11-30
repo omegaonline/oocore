5e9238e7c374099263d67b0e74d9643779fa5697
Rick Taylor <rick@tropicalstormsoftware.com>
2005-11-30 17:16:10 +0000

    Didnt rename a file properly

--- src/OOCore/ProxyStub_Types.h
+++ src/OOCore/ProxyStub_Types.h
@@ -5,7 +5,7 @@
 
 #include "./OOCore_Impl.h"
 
-namespace OOProxyStub
+namespace OOCore
 {
 namespace Impl
 {
@@ -73,8 +73,11 @@ namespace Impl
 	{
 	public:
 		array_t(OOObject::uint32_t* c) :
-		  m_data(0), m_p(0), m_count(c)
-		{ }
+		  m_count(c)
+		{
+			m_data = static_cast<T*>(OOObject::Alloc(sizeof(T)*(*m_count)));
+			m_p=m_data;
+		}
 
         array_t(T** ar, OOObject::uint32_t* c)	: 
 		  m_data(0), m_p(*ar), m_count(c)
@@ -94,15 +97,12 @@ namespace Impl
 		
 		int read(OOCore::Impl::InputStream_Wrapper& in)
 		{
-			m_data = static_cast<T*>(OOObject::Alloc(sizeof(T)*(*m_count)));
-			if (!m_data) return -1;
+			if (!m_p) return -1;
 				
 			for (OOObject::uint32_t i=0;i<*m_count;++i)
 			{
-				if (in.read(m_data[i])!=0) return -1;
+				if (in.read(m_p[i])!=0) return -1;
 			}
-			if (m_p) OOObject::Free(m_p);
-			m_p = m_data;
 			return 0;
 		}
 
@@ -202,7 +202,7 @@ namespace Impl
 			return m_obj;
 		}
 
-		int read(OOCore::ProxyStubManager* manager, OOCore::Impl::InputStream_Wrapper& in)
+		int read_ps(OOCore::ProxyStubManager* manager, OOCore::Impl::InputStream_Wrapper& in)
 		{
 			OOObject::cookie_t key;
 			if (in.read(key) != 0) 
@@ -211,7 +211,7 @@ namespace Impl
 			return manager->CreateProxy(m_iid,key,reinterpret_cast<OOObject::Object**>(&m_obj));
 		}
 
-		int write(OOCore::ProxyStubManager* manager, OOCore::OutputStream* out)
+		int write_ps(OOCore::ProxyStubManager* manager, OOCore::OutputStream* out)
 		{
 			return manager->CreateStub(m_iid,m_obj,out);
 		}
@@ -238,7 +238,7 @@ namespace Impl
 			return &m_obj;
 		}
 
-		int read(OOCore::ProxyStubManager* manager, OOCore::Impl::InputStream_Wrapper& in)
+		int read_ps(OOCore::ProxyStubManager* manager, OOCore::Impl::InputStream_Wrapper& in)
 		{
 			OOObject::cookie_t key;
 			if (in.read(key) != 0) 
@@ -247,7 +247,7 @@ namespace Impl
 			return manager->CreateProxy(m_iid,key,&m_obj);
 		}
 		
-		int write(OOCore::ProxyStubManager* manager, OOCore::OutputStream* out)
+		int write_ps(OOCore::ProxyStubManager* manager, OOCore::OutputStream* out)
 		{
 			return manager->CreateStub(m_iid,m_obj,out);
 		}
@@ -258,10 +258,10 @@ namespace Impl
 	};
 
 	template <class T>
-	class stub_param_t
+	class param_t
 	{
 	public:
-		stub_param_t(OOCore::InputStream* input)
+		param_t(OOCore::InputStream* input)
 		{
 			m_failed = (OOCore::Impl::InputStream_Wrapper(input).read(m_t)!=0);
 		}
@@ -285,14 +285,14 @@ namespace Impl
 	};
 
 	template <class T>
-	class stub_param_t<T*>
+	class param_t<T*>
 	{
 	public:
-		stub_param_t() :
+		param_t() :
 		  m_failed(false)
 		{ }
 
-		stub_param_t(OOCore::InputStream* input)
+		param_t(OOCore::InputStream* input)
 		{
 			m_failed = (OOCore::Impl::InputStream_Wrapper(input).read(m_t)!=0);
 		}
@@ -318,10 +318,10 @@ namespace Impl
 	};
 
 	template <class T>
-	class stub_param_t<array_t<T*> >
+	class param_t<array_t<T*> >
 	{
 	public:
-		stub_param_t(OOCore::InputStream* input, const OOObject::uint32_t c) :
+		param_t(OOCore::InputStream* input, const OOObject::uint32_t c) :
 		  m_arr(c), m_failed(false)
 		{
 			m_failed = (m_arr.read(OOCore::Impl::InputStream_Wrapper(input))!=0);
@@ -343,14 +343,14 @@ namespace Impl
 	};
 
 	template <class T>
-	class stub_param_t<array_t<T**> >
+	class param_t<array_t<T**> >
 	{
 	public:
-		stub_param_t(stub_param_t<OOObject::uint32_t*>& c) :
+		param_t(param_t<OOObject::uint32_t*>& c) :
 		  m_arr(c), m_failed(false)
 		{ }
 
-		stub_param_t(OOCore::InputStream* input, stub_param_t<OOObject::uint32_t*>& c) :
+		param_t(OOCore::InputStream* input, param_t<OOObject::uint32_t*>& c) :
 		  m_arr(c), m_failed(false)
 		{
 			m_failed = (m_arr.read(OOCore::Impl::InputStream_Wrapper(input))!=0);
@@ -377,10 +377,10 @@ namespace Impl
 	};
 
 	template <class T>
-	class stub_param_t<string_t<T> >
+	class param_t<string_t<T> >
 	{
 	public:
-		stub_param_t(OOCore::InputStream* input) :
+		param_t(OOCore::InputStream* input) :
 		  m_failed(false)
 		{
 			m_failed = (m_str.read(OOCore::Impl::InputStream_Wrapper(input))!=0);
@@ -402,17 +402,17 @@ namespace Impl
 	};
 
 	template <class T>
-	class stub_param_t<object_t<T*> >
+	class param_t<object_t<T*> >
 	{
 	public:
-		stub_param_t(const OOObject::guid_t& iid) :
+		param_t(const OOObject::guid_t& iid) :
 		  m_obj(iid), m_failed(false)
 		{ }
 
-		stub_param_t(OOCore::InputStream* input, OOCore::ProxyStubManager* manager, const OOObject::guid_t& iid) :
+		param_t(OOCore::InputStream* input, OOCore::ProxyStubManager* manager, const OOObject::guid_t& iid) :
 		  m_obj(iid), m_failed(false)
 		{
-			m_failed = (m_obj.read(manager,OOCore::Impl::InputStream_Wrapper(input))!=0);
+			m_failed = (m_obj.read_ps(manager,OOCore::Impl::InputStream_Wrapper(input))!=0);
 		}
 
 		operator T*()
@@ -431,10 +431,10 @@ namespace Impl
 	};
 
 	template <class T>
-	class stub_param_t<object_t<T**> >
+	class param_t<object_t<T**> >
 	{
 	public:
-		stub_param_t(const OOObject::guid_t& iid) :
+		param_t(const OOObject::guid_t& iid) :
 		  m_obj(iid)
 		{ }
 
@@ -445,102 +445,11 @@ namespace Impl
 
 		int respond(OOCore::OutputStream* output, OOCore::ProxyStubManager* manager)
 		{
-			return m_obj.write(manager,output);
+			return m_obj.write_ps(manager,output);
 		}
 
 	private:
 		object_t<T**> m_obj;
-	};
-
-	class invoker_t
-	{
-	public:
-		template <class T, class I>
-		static int Invoke(T* pT, I* iface, OOCore::ProxyStubManager* manager, OOObject::uint32_t method, OOObject::int32_t& ret_code, OOCore::InputStream* input, OOCore::OutputStream* output)
-		{
-			OOCORE_PS_DECLARE_INVOKE_TABLE()
-		}
-	};
-
-	class marshaller_t
-	{
-	public:
-		marshaller_t();
-		marshaller_t(OOCore::ProxyStubManager* manager, OOObject::bool_t sync, OOCore::OutputStream* output, OOObject::uint32_t trans_id);
-
-		template <class T>
-		marshaller_t& operator <<(const T& val)
-		{
-			if (!m_failed)
-				m_failed = (m_out.write(val)!=0);
-			return *this;
-		}
-
-		template <class T>
-		marshaller_t& operator <<(T* val)
-		{
-			if (!m_failed)
-				m_failed = (m_out.write(*val)!=0);
-			return *this;
-		}
-
-		template <class T>
-		marshaller_t& operator <<(array_t<T>& val)
-		{
-			if (!m_failed)
-				m_failed = (val.write(m_out)!=0);
-			return *this;
-		}
-
-		template <class T>
-		marshaller_t& operator <<(string_t<T>& val)
-		{
-			if (!m_failed)
-				m_failed = (val.write(m_out)!=0);
-			return *this;
-		}
-
-		template <class T>
-		marshaller_t& operator <<(object_t<T*>& val)
-		{
-			if (!m_failed)
-				m_failed = (val.write(m_manager,m_out)!=0);
-			return *this;
-		}
-		
-		template <class T>
-		marshaller_t& operator >>(T* val)
-		{
-			if (!m_failed)
-				m_failed = (m_in.read(*val)!=0);
-			return *this;
-		}
-
-		template <class T>
-		marshaller_t& operator >>(array_t<T**>& val)
-		{
-			if (!m_failed)
-				m_failed = (val.read(m_in)!=0);
-			return *this;
-		}
-
-		template <class T>
-		marshaller_t& operator >>(object_t<T**>& val)
-		{
-			if (!m_failed)
-				m_failed = (val.read(m_manager,m_in)!=0);
-			return *this;
-		}
-
-		OOObject::int32_t send_and_recv();
-
-	private:
-		OOCore::Impl::InputStream_Wrapper	m_in;
-		OOCore::Impl::OutputStream_Wrapper	m_out;
-		bool						m_failed;
-		OOCore::Object_Ptr<OOCore::ProxyStubManager> m_manager;
-		const OOObject::bool_t			m_sync;
-		const OOObject::uint32_t		m_trans_id;
 	};
 };
 };

b19cd50dbebad9de453f9cde220f06472cfe3043
Rick Taylor <rick@ubuntu-vmware.(none)>
2009-06-18 20:45:19 +0100

    More linux fixes

---
+++ m4/oo_sockopt.m4
@@ -0,0 +1,27 @@
+
+AC_DEFUN([OO_CHECK_SOCKOPT],
+[
+	AC_LANG_PUSH([C])
+	m4_foreach_w([oo_check_sockop],[$1],
+	[
+		AC_MSG_CHECKING([for oo_check_sockop socket option])
+
+		AC_COMPILE_IFELSE(
+			[
+				AC_LANG_PROGRAM([[ #include <sys/socket.h> ]],
+				[
+					setsockopt(12, SOL_SOCKET, oo_check_sockop, "nothing", 1);
+				]
+			)
+			],
+			[
+				AC_MSG_RESULT([yes])
+				AC_DEFINE_UNQUOTED([HAVE_]AS_TR_CPP(oo_check_sockop), [1], Define to 1 if you have the oo_check_sockop socket option)
+			],
+			[
+				AC_MSG_RESULT([no])
+			]
+		)
+	])
+	AC_LANG_POP([C])
+])
---
+++ src/OOBase/ProactorEv.h
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2009 Rick Taylor
+//
+// This file is part of OOSvrBase, the Omega Online Base library.
+//
+// OOSvrBase is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OOSvrBase is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OOSvrBase.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+#ifndef OOSVRBASE_PROACTOR_EV_H_INCLUDED_
+#define OOSVRBASE_PROACTOR_EV_H_INCLUDED_
+
+namespace OOSvrBase
+{
+	class ProactorImpl
+	{
+	public:
+		ProactorImpl();
+		~ProactorImpl();
+
+		OOBase::Socket* accept_local(Acceptor* handler, const std::string& path, int* perr, SECURITY_ATTRIBUTES*);
+
+		AsyncSocket* attach_socket(IOHandler* handler, int* perr, OOBase::Socket* sock);
+
+	private:
+		ProactorImpl(const ProactorImpl&) {}
+		ProactorImpl& operator = (const ProactorImpl&) { return *this; }
+	};
+}
+
+#endif // OOSVRBASE_PROACTOR_EV_H_INCLUDED_

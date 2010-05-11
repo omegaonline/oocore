aeb52eb069e2109e1f78835f93f87eafab3520fd
Rick Taylor <rick@tropicalstormsoftware.com>
2010-05-11 14:28:58 +0100

    Updated bootstrap and makefiles
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 echo Running autotools...
 aclocal -I m4 && \
 autoheader && \
-libtoolize --copy --ltdl --force && \
+libtoolize --copy --force --no-warn && \
 automake --foreign --add-missing --copy && \
 autoconf

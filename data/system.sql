67643f6d7d9eb0e5bd6018d3c147beb5a158e7de
Rick Taylor <rick@tropicalstormsoftware.com>
2010-08-21 11:44:57 +0100

    System sql tidy up
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- data/system.sql
+++ data/system.sql
@@ -7,6 +7,7 @@ BEGIN TRANSACTION;
 		'The system configuration key',
 		0,7
 	);
+	
 	INSERT INTO RegistryKeys ( Id,Name,Description,Parent,Access) VALUES 
 	(
 		2,
@@ -24,28 +25,53 @@ BEGIN TRANSACTION;
 	INSERT INTO RegistryKeys ( Id,Name,Description,Parent,Access) VALUES 
 	(
 		4,
+		'OIDs',
+		'',
+		3,6
+	);
+	INSERT INTO RegistryKeys ( Id,Name,Description,Parent,Access) VALUES 
+	(
+		5,
+		'Applications',
+		'Applications store their configuration beneath this key',
+		2,6
+	);
+		
+	INSERT INTO RegistryKeys ( Id,Name,Description,Parent,Access) VALUES 
+	(
+		6,
 		'All Users',
 		'A key shared between all users',
 		0,6
 	);
 	INSERT INTO RegistryKeys ( Id,Name,Description,Parent,Access) VALUES 
 	(
-		5,
+		7,
 		'Objects',
 		'',
-		4,6
+		6,6
 	);
-	INSERT INTO RegistryKeys ( Name,Description,Parent,Access) VALUES 
+	INSERT INTO RegistryKeys ( Id,Name,Description,Parent,Access) VALUES 
 	(
+		8,
 		'OIDs',
 		'',
-		5,6
+		7,6
 	);
-	INSERT INTO RegistryKeys ( Name,Description,Parent,Access) VALUES 
+	INSERT INTO RegistryKeys ( Id,Name,Description,Parent,Access) VALUES 
 	(
+		9,
 		'Applications',
 		'Applications store their configuration beneath this key',
-		,6
+		6,6
+	);
+	
+	INSERT INTO RegistryKeys ( Id,Name,Description,Parent,Access) VALUES 
+	(
+		10,
+		'Services',
+		'System services run at startup',
+		1,7
 	);
 	
 COMMIT;

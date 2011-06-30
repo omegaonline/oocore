ed29963f8a6aa664ae79f7eb638cbdab4ac56f98
Rick Taylor <rick@tropicalstormsoftware.com>
2011-06-30 11:53:41 +0100

    Split the launch code out of UserSession.cpp ready for tackling issue 4.
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

---
+++ src/OOCore/Launch.cpp
@@ -0,0 +1,282 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2011 Rick Taylor
+//
+// This file is part of OOCore, the Omega Online Core library.
+//
+// OOCore is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OOCore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OOCore.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+#include "OOCore_precomp.h"
+
+#include "UserSession.h"
+#include "Activation.h"
+
+using namespace Omega;
+using namespace OTL;
+
+namespace
+{
+	void parse_args(const string_t& str, OOBase::Table<string_t,string_t>& args)
+	{
+		// Split out individual args
+		for (size_t start = 0;;)
+		{
+			// Skip leading whitespace
+			while (start < str.Length() && (str[start] == L'\t' || str[start] == L' '))
+				++start;
+
+			if (start == str.Length())
+				return;
+
+			// Find the next linefeed
+			size_t end = str.Find(L',',start);
+
+			// Trim trailing whitespace
+			size_t valend = (end == string_t::npos ? str.Length() : end);
+			while (valend > start && (str[valend-1] == L'\t' || str[valend-1] == L' '))
+				--valend;
+
+			if (valend > start)
+			{
+				string_t strKey, strValue;
+
+				// Split on first =
+				size_t eq = str.Find(L'=',start);
+				if (eq != string_t::npos)
+				{
+					// Trim trailing whitespace before =
+					size_t keyend = eq;
+					while (keyend > start && (str[keyend-1] == L'\t' || str[keyend-1] == L' '))
+						--keyend;
+
+					if (keyend > start)
+					{
+						strKey = str.Mid(start,keyend-start);
+
+						// Skip leading whitespace after =
+						size_t valpos = eq+1;
+						while (valpos < valend && (str[valpos] == L'\t' || str[valpos] == L' '))
+							++valpos;
+
+						if (valpos < valend)
+							strValue = str.Mid(valpos,valend-valpos);
+					}
+				}
+				else
+				{
+					strKey = str.Mid(start,valend-start);
+					strValue = L"true";
+				}
+
+				if (!strKey.IsEmpty())
+				{
+					int err = args.replace(strKey,strValue);
+					if (err != 0)
+						OMEGA_THROW(err);
+				}
+			}
+
+			if (end == string_t::npos)
+				return;
+
+			start = end + 1;
+		}
+	}
+	
+	void discover_server_port(bool& bStandalone, OOBase::LocalString& strPipe)
+	{
+		int err = strPipe.getenv("OMEGA_SESSION_ADDRESS");
+		if (err != 0)
+			OMEGA_THROW(err);
+			
+		if (!strPipe.empty())
+			return;
+			
+	#if defined(_WIN32)
+		const char* name = "OmegaOnline";
+	#else
+		void* TODO;
+		
+		const char* name = "/tmp/nowhere";
+	#endif
+
+		OOBase::SmartPtr<OOBase::Socket> local_socket = OOBase::Socket::connect_local(name,&err);
+		if (!local_socket)
+		{
+			if (err == ENOENT && bStandalone)
+				return;
+
+			ObjectPtr<IException> ptrE;
+			ptrE.Attach(ISystemException::Create(err));
+			throw IInternalException::Create("Failed to connect to network daemon","Omega::Initialize",0,NULL,ptrE);
+		}
+		bStandalone = false;
+
+		OOBase::CDRStream stream;
+
+		// Send version information
+		uint32_t version = (OOCORE_MAJOR_VERSION << 24) | (OOCORE_MINOR_VERSION << 16) | OOCORE_PATCH_VERSION;
+		if (!stream.write(version))
+			OMEGA_THROW(stream.last_error());
+		
+		err = local_socket->send(stream.buffer());
+		if (err)
+			OMEGA_THROW(err);
+			
+		stream.reset();
+
+		// We know a CDRStream writes strings as a 4 byte length followed by the character data
+		size_t mark = stream.buffer()->mark_rd_ptr();
+		local_socket->recv(stream.buffer(),4,&err);
+		if (err != 0)
+			OMEGA_THROW(err);
+
+		uint32_t len = 0;
+		if (!stream.read(len))
+			OMEGA_THROW(stream.last_error());
+
+		local_socket->recv(stream.buffer(),len,&err);
+		if (err != 0)
+			OMEGA_THROW(err);
+
+		// Now reset rd_ptr and read the string
+		stream.buffer()->mark_rd_ptr(mark);
+
+		if (!stream.read(strPipe))
+			OMEGA_THROW(stream.last_error());
+	}
+}
+
+void OOCore::UserSession::start(const string_t& strArgs)
+{
+	OOBase::Table<string_t,string_t> args;
+	parse_args(strArgs,args);
+
+	bool bStandalone = false;
+	bool bStandaloneAlways = false;
+	size_t i = args.find(L"standalone");
+	if (i != args.npos)
+	{
+		if (*args.at(i) == L"true")
+			bStandalone = true;
+		else if (*args.at(i) == L"always")
+		{
+			bStandalone = true;
+			bStandaloneAlways = true;
+		}
+	}
+
+	OOBase::LocalString strPipe;
+	if (!bStandaloneAlways)
+		discover_server_port(bStandalone,strPipe);
+
+	int err = 0;
+	if (!bStandalone)
+	{
+		// Connect up to the user process...
+		OOBase::timeval_t wait(5);
+		OOBase::Countdown countdown(&wait);
+		do
+		{
+			m_stream = OOBase::Socket::connect_local(strPipe.c_str(),&err,&wait);
+			if (!err || err != ENOENT)
+				break;
+
+			// We ignore the error, and try again until we timeout
+			countdown.update();
+		}
+		while (wait != OOBase::timeval_t::Zero);
+
+		if (err)
+			OMEGA_THROW(err);
+
+		// Send version information
+		uint32_t version = (OOCORE_MAJOR_VERSION << 24) | (OOCORE_MINOR_VERSION << 16) | OOCORE_PATCH_VERSION;
+		if ((err = m_stream->send(version)) != 0)
+			OMEGA_THROW(err);
+
+		// Read our channel id
+		if ((err = m_stream->recv(m_channel_id)) != 0)
+			OMEGA_THROW(err);
+
+		// Spawn off the io worker thread
+		m_worker_thread.run(io_worker_fn,this);
+	}
+
+	// Create the zero compartment
+	OOBase::SmartPtr<Compartment> ptrZeroCompt = new (std::nothrow) Compartment(this);
+	if (!ptrZeroCompt)
+		OMEGA_THROW_NOMEM();
+	else
+	{
+		OOBase::Guard<OOBase::RWMutex> guard(m_lock);
+
+		if ((err = m_mapCompartments.replace(0,ptrZeroCompt)) != 0)
+			OMEGA_THROW(err);
+
+		ptrZeroCompt->set_id(0);
+	}
+
+	// Remove standalone support eventually...
+	void* ISSUE_4;
+
+	ObjectPtr<IInterProcessService> ptrIPS;
+	if (!bStandalone)
+	{
+		// Create a new object manager for the user channel on the zero compartment
+		ObjectPtr<Remoting::IObjectManager> ptrOM = ptrZeroCompt->get_channel_om(m_channel_id & 0xFF000000);
+
+		// Create a proxy to the server interface
+		IObject* pIPS = 0;
+		ptrOM->GetRemoteInstance(OID_InterProcessService,Activation::Library | Activation::DontLaunch,OMEGA_GUIDOF(IInterProcessService),pIPS);
+
+		ptrIPS.Attach(static_cast<IInterProcessService*>(pIPS));
+	}
+	else
+	{
+		// Load up OOSvrLite and get the IPS from there...
+		int err = m_lite_dll.load("oosvrlite");
+		if (err != 0)
+			OMEGA_THROW(err);
+
+		typedef const System::Internal::SafeShim* (OMEGA_CALL *pfnOOSvrLite_GetIPS_Safe)(System::Internal::marshal_info<IInterProcessService*&>::safe_type::type OOSvrLite_GetIPS_RetVal, System::Internal::marshal_info<const string_t&>::safe_type::type args);
+
+		pfnOOSvrLite_GetIPS_Safe pfn = (pfnOOSvrLite_GetIPS_Safe)(m_lite_dll.symbol("OOSvrLite_GetIPS_Safe"));
+		if (!pfn)
+			OMEGA_THROW("Corrupt OOSvrLite");
+
+		IInterProcessService* pIPS = 0;
+		const System::Internal::SafeShim* pSE = (*pfn)(System::Internal::marshal_info<IInterProcessService*&>::safe_type::coerce(pIPS),System::Internal::marshal_info<const string_t&>::safe_type::coerce(strArgs));
+		if (pSE)
+			System::Internal::throw_correct_exception(pSE);
+
+		ptrIPS.Attach(pIPS);
+	}
+
+	// Register locally...
+	m_nIPSCookie = OOCore_RegisterIPS(ptrIPS);
+	
+	// Now set our pipe name as an env var
+	if (!strPipe.empty())
+	{
+#if defined(_WIN32)
+		if (strPipe.printf("OMEGA_SESSION_ADDRESS=%s",strPipe.c_str()) == 0)
+			_putenv(strPipe.c_str());
+#else
+		setenv("OMEGA_SESSION_ADDRESS",strPipe.c_str(),1);
+#endif
+	}
+}

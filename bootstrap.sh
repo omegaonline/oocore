334c0e9304e983d627ed559023062c3712f47219
Rick Taylor <rick@tropicalstormsoftware.com>
2012-01-16 12:04:46 +0000

    Cleanup makefiles
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,6 @@
 #! /bin/sh
 echo Running autotools...
 
-if test ! -d "./oobase"; then
-	echo You need to link oobase to ./oobase, try ln -s
-fi
-
 aclocal -I m4 && \
 autoheader && \
 libtoolize --force --no-warn && \

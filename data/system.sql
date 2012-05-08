d888416c3f05237363d998bc99c6c5a0ea0146f7
Rick Taylor <rick@tropicalstormsoftware.com>
2012-05-08 11:43:48 +0100

    Add the Surrogate registration to the system.sql and reassign ids
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- data/system.sql
+++ data/system.sql
@@ -15,32 +15,40 @@ BEGIN TRANSACTION;
 	    INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 5, 'Applications', 2 );
 	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 5, 6 );
 	        
-	  INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 12, 'Services', 1 );
-	      INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 12, 7 );
+	  INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 6, 'Services', 1 );
+	      INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 6, 7 );
 		
-	INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 6, 'All Users', 0 );
-	    INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 6, 6 );
+	INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 7, 'All Users', 0 );
+	    INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 7, 6 );
 	
-	  INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 7, 'Objects', 6 );
-	      INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 7, 6 );
+	  INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 8, 'Objects', 7 );
+	      INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 8, 6 );
 	
-	    INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 8, 'Omega.Activation.RunningObjectTable', 7 );
-	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 8, 6 );
-	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( 'OID', 8, '{F67F5A41-BA32-48C9-BFD2-7B3701984DC8}' );
-	
-	    INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 9, 'Omega.Registry', 7 );
+	    INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 9, 'Omega.Activation.RunningObjectTable', 8 );
 	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 9, 6 );
-	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( 'OID', 9, '{EAAC4365-9B65-4C3C-94C2-CC8CC3E64D74}' );
-
-	    INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 10, 'OIDs', 7 );
+	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( 'OID', 9, '{F67F5A41-BA32-48C9-BFD2-7B3701984DC8}' );
+	
+	    INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 10, 'Omega.Registry', 8 );
 	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 10, 6 );
+	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( 'OID', 10, '{EAAC4365-9B65-4C3C-94C2-CC8CC3E64D74}' );
+	        
+	    INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 11, 'Omega.Surrogate', 8 );
+	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 11, 6 );
+	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( 'OID', 11, '{D063D32C-FB9A-004A-D2E5-BB5451808FF5}' );
+	        
+	    INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 12, 'Omega.SingleSurrogate', 8 );
+	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 12, 6 );
+	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( 'OID', 12, '{22DC1376-4905-D9DD-1B63-2096C487E5A3}' );
 
-	  INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 11, 'Applications', 6 );
-	      INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 11, 6 ); 
+	    INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 13, 'OIDs', 8 );
+	        INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 13, 6 );
 
-	INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 13, '.links', 0 );
-	    INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 13, 7 );
-	    INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( 'Local User', 13, 'system:user/' );
+	  INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 14, 'Applications', 7 );
+	      INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 14, 6 ); 
+
+	INSERT INTO RegistryKeys ( Id,Name,Parent) VALUES ( 15, '.links', 0 );
+	    INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( '.access', 15, 7 );
+	    INSERT INTO RegistryValues ( Name,Parent,Value) VALUES ( 'Local User', 15, 'system:user/' );
 	
 COMMIT;
 

fe16da4621a40053503c937469993a285970139f
Rick Taylor <rick@tropicalstormsoftware.com>
2012-06-08 14:29:03 +0100

    Updates to support 32bit and 64bit simulataneously on Win64
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- src/OOServer/libdb/make_db.bat
+++ src/OOServer/libdb/make_db.bat
@@ -1,7 +1,7 @@
-del %2system.regdb
+del /Q %2system.regdb
 %1\sqlite3.exe %2system.regdb < %3data\common.sql
 %1\sqlite3.exe %2system.regdb < %3data\system.sql
 
-del %2user_template.regdb
+del /Q %2user_template.regdb
 %1\sqlite3.exe %2user_template.regdb < %3data\common.sql
 %1\sqlite3.exe %2user_template.regdb < %3data\user_template.sql

08fa657c84cd8128097e25ff7e151ec45dfd1500
Rick Taylor <rick@tropicalstormsoftware.com>
2012-06-23 13:17:02 +0100

    Use the correct debug event in the host process
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- src/OOServer/HostMain.cpp
+++ src/OOServer/HostMain.cpp
@@ -131,7 +131,7 @@ int main(int argc, char* argv[])
 	// If this event exists, then we are being debugged
 	{
 		// Scope it...
-		OOBase::Win32::SmartHandle hDebugEvent(OpenEventW(EVENT_ALL_ACCESS,FALSE,L"Global\\OOSERVER_DEBUG_MUTEX"));
+		OOBase::Win32::SmartHandle hDebugEvent(OpenEventW(EVENT_ALL_ACCESS,FALSE,L"Local\\OOCORE_DEBUG_MUTEX"));
 		if (hDebugEvent)
 		{
 			// Wait for a bit, letting the caller attach a debugger

a629317f7bedb942fc1683c2b72a4925f1c634e2
Rick Taylor <rick@tropicalstormsoftware.com>
2013-03-09 17:53:11 +0000

    Update GPL text, it uses the wrong project name
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- src/OOServer/RegistryManager.cpp
+++ src/OOServer/RegistryManager.cpp
@@ -2,20 +2,20 @@
 //
 // Copyright (C) 2013 Rick Taylor
 //
-// This file is part of OOServer, the Omega Online Server application.
+// This file is part of OOSvrReg, the Omega Online registry server.
 //
-// OOServer is free software: you can redistribute it and/or modify
+// OOSvrReg is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// OOServer is distributed in the hope that it will be useful,
+// OOSvrReg is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with OOServer.  If not, see <http://www.gnu.org/licenses/>.
+// along with OOSvrReg.  If not, see <http://www.gnu.org/licenses/>.
 //
 ///////////////////////////////////////////////////////////////////////////////////
 

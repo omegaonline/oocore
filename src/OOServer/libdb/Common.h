1f26168a6ca958aceb70ac34c78ebb1a1e3554c5
Rick Taylor <rick@tropicalstormsoftware.com>
2013-04-15 12:37:53 +0100

    Fix build for latest OOBase
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- src/OOServer/libdb/Common.h
+++ src/OOServer/libdb/Common.h
@@ -33,6 +33,13 @@
 #include <oocore-autoconf.h>
 #endif
 
+#if defined(_WIN32)
+	// Remove the unistd include - we are windows
+	#if defined(HAVE_UNISTD_H)
+	#undef HAVE_UNISTD_H
+	#endif
+#endif
+
 #include "../../include/Omega/internal/base_types.h"
 
 #endif // OOCORE_LIBDB_COMMON_H_INCLUDED_
